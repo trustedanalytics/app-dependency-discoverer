74e790e0f702836f14dd1aa9610b4f932a860594
Marek Tomczewski <marek.tomczewski@intel.com>
2016-06-06 14:13:08 +0200

    Increased bumpversion.
    Signed-off-by: Marek Tomczewski <marek.tomczewski@intel.com>

--- main.go
+++ main.go
@@ -18,7 +18,7 @@
 //
 // This application can discover dependencies of application stack with provided root GUID.
 //
-//     Version: 0.1.3
+//     Version: 0.1.4
 //
 // swagger:meta
 package main
